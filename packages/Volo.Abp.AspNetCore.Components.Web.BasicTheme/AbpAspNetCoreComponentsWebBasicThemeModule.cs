41341cc3634fb7084d0d239a6df0768dfafe6318
Ahmet <yellow.dragon.cloud@gmail.com>
2022-03-29 23:43:57 +0300

    apply part 1

--- packages/Volo.Abp.AspNetCore.Components.Web.BasicTheme/AbpAspNetCoreComponentsWebBasicThemeModule.cs
+++ packages/Volo.Abp.AspNetCore.Components.Web.BasicTheme/AbpAspNetCoreComponentsWebBasicThemeModule.cs
@@ -1,12 +1,17 @@
-﻿using Volo.Abp.AspNetCore.Components.Web.Theming;
+﻿using MudBlazor.Services;
+using Volo.Abp.AspNetCore.Components.Web.Theming;
 using Volo.Abp.Modularity;
 
 namespace Volo.Abp.AspNetCore.Components.Web.BasicTheme;
 
 [DependsOn(
     typeof(AbpAspNetCoreComponentsWebThemingModule)
-    )]
+)]
 public class AbpAspNetCoreComponentsWebBasicThemeModule : AbpModule
 {
-
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        base.ConfigureServices(context);
+        context.Services.AddMudServices();
+    }
 }
--- packages/Volo.Abp.AspNetCore.Components.WebAssembly.BasicTheme/BasicThemeBundleContributor.cs
+++ packages/Volo.Abp.AspNetCore.Components.WebAssembly.BasicTheme/BasicThemeBundleContributor.cs
@@ -6,11 +6,14 @@ public class BasicThemeBundleContributor : IBundleContributor
 {
     public void AddScripts(BundleContext context)
     {
-
+        context.Add("_content/MudBlazor/MudBlazor.min.js");
     }
 
     public void AddStyles(BundleContext context)
     {
         context.Add("_content/Volo.Abp.AspNetCore.Components.Web.BasicTheme/libs/abp/css/theme.css");
+
+        context.Add("https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap");
+        context.Add("_content/MudBlazor/MudBlazor.min.css");
     }
 }
